b0a6d00b0e6e6192ecdef7d5175d03b6608417ec
homholueng <homholueng@gmail.com>
2022-04-20 10:05:14 +0800

    Update manage.py

--- template/{{cookiecutter.project_name}}/bin/manage.py
+++ template/{{cookiecutter.project_name}}/bin/manage.py
@@ -18,6 +18,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bk_plugin_runtime.settings")
+    os.environ.setdefault("BK_APP_CONFIG_PATH", "bk_plugin_runtime.config")
 
     from django.core.management import execute_from_command_line
 
