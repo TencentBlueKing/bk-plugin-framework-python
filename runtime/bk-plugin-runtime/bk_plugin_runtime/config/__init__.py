60600c31f1747c7f52619360b8cfa2a545e09b35
dengyh <dengyh071@gmail.com>
2025-08-28 11:51:44 +0800

    minor: runtime release V2.1.0rc3 # Reviewed, transaction id: 55861

--- runtime/bk-plugin-runtime/bk_plugin_runtime/config/__init__.py
+++ runtime/bk-plugin-runtime/bk_plugin_runtime/config/__init__.py
@@ -18,7 +18,10 @@ import os
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
+import django
 from blueapps.core.celery import celery_app
+from django.utils.functional import cached_property
+from django.db.backends.mysql.features import DatabaseFeatures
 
 # app 基本信息
 
@@ -47,3 +50,20 @@ BK_URL = None
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 兼容低版本 MySQL
+class PatchFeatures:
+    @cached_property
+    def minimum_database_version(self):
+        django_version = django.VERSION
+        if self.connection.mysql_is_mariadb:
+            return 10, 4
+        elif django_version < (4, 0):
+            import pymysql
+            pymysql.install_as_MySQLdb()
+            return 5, 7
+        else:
+            return 8, 0
+
+# 目前 Django 仅是对 5.7 做了软性的不兼容改动，在没有使用 8.0 特异的功能时，对 5.7 版本的使用无影响
+DatabaseFeatures.minimum_database_version = PatchFeatures.minimum_database_version
